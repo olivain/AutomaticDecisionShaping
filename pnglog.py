de405775a3b24923dd51bbd33818cfb97e6637be
olivain <olivain@users.noreply.github.com>
2024-04-24 01:18:26 +0200

    Update pnglog.py

--- pnglog.py
+++ pnglog.py
@@ -2,11 +2,15 @@ import numpy as np
 import cv2
 import random
 import os
-
-
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import threading
 
 def stack_images_on_top(existing_file, new_image):
-    existing_image = cv2.imread(existing_file, cv2.IMREAD_GRAYSCALE)
+
+    existing_image = None
+    if os.path.exists(existing_file):
+        existing_image = cv2.imread(existing_file, cv2.IMREAD_GRAYSCALE)
+
     if existing_image is None:
         existing_image = np.ones_like(new_image)
 
@@ -14,10 +18,9 @@ def stack_images_on_top(existing_file, new_image):
         existing_image = cv2.cvtColor(existing_image, cv2.COLOR_BGR2GRAY)
     if len(new_image.shape) > 2:
         new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
+
     stacked_image = np.vstack((new_image, existing_image))
     cv2.imwrite(existing_file, stacked_image)
-
-
 
 def display_spaced_stacked_copies(image_file, num_copies, orientation='horizontal', target_width=800, column_spacing_cm=2):
     tmp = cv2.imread(image_file)
@@ -48,3 +51,48 @@ def display_spaced_stacked_copies(image_file, num_copies, orientation='horizonta
     stacked_image_resized = cv2.resize(stacked_image, (target_width, target_height))
 
     return stacked_image_resized
+
+def crop_image_from_top(image_path,desired_height):
+    image = cv2.imread(image_path)
+    height, width, _ = image.shape
+    if height > desired_height:
+        cropped_image = image[0:desired_height, 0:width]
+        cv2.imwrite(image_path, cropped_image)
+        return True
+    return False
+
+
+class RequestHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.path = "/index.html"
+        return SimpleHTTPRequestHandler.do_GET(self)
+
+def start_server():
+    server_port = 8000
+    server_address = ('', server_port) 
+    httpd = HTTPServer(server_address, RequestHandler)
+    print(f'Server running on port {server_port}...')
+    httpd.serve_forever()
+
+def initializeHttpOutput(nb):
+    generate_html(nb)
+    server_thread = threading.Thread(target=start_server)
+    server_thread.daemon = True
+    server_thread.start()
+    print("Http output started: http://localhost:8000")
+
+
+def generate_html(png_files_count):
+    html_content = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Automatic Decision Shaping (simulation 1)</title><style>body,html{background-color:gray;height:100%;margin:0;display:flex;flex-direction:column;justify-content:center}.container{display:flex;flex-direction:column;align-items:center;width:100%}.images{display:flex;flex-direction:row}.boxes{display:flex;justify-content:center}.image{width:10vw;margin-left:calc(2.5vw + 10px);margin-right:calc(2.5vw + 10px);box-shadow:8px 8px 16px rgb(0 0 0 / .4)}.box{width:15vw;height:50px;background-color:#000;height:170px;margin:10px;box-shadow:8px 8px 16px rgb(0 0 0 / .4)}.container img:hover{transform:translateY(10px)}</style></head><body><div class="container"><div class="boxes">"""
+    for i in range(png_files_count):
+        html_content += "<div class=\"box\"></div>"
+    html_content += """</div></div><div class="container" style="height: 100%; margin-top: -80px;"><div class="images">"""
+    for i in range(png_files_count):
+        path = f"columns/{i}.png"
+        r = random.randint(99,99999)
+        im = f"columns/{i}.png?t={r}"
+        html_content += f" <img src='{im}' class='image' alt='Image {i}'>\n"
+    html_content += """</div></div><script>document.addEventListener("DOMContentLoaded",function(){console.log("Page loaded");setInterval(reloadImages,5000)});function reloadImages(){const images=document.querySelectorAll('.image');images.forEach(image=>{const timestamp=new Date().getTime();const src=image.src;image.src=src.includes('?')?src.split('?')[0]+'?'+timestamp:src+'?t='+timestamp;console.log("Reloading image:",image.src)})}</script></body></html>"""
+    with open("index.html", "w") as file:
+        file.write(html_content)
