34e20acd13a5b9f146634644b814e4d6d573b3a2
olivain <olivain@users.noreply.github.com>
2024-04-29 16:10:32 +0200

    Update printer.py

--- printer.py
+++ printer.py
@@ -1,15 +1,31 @@
-from escpos.printer import Usb
+from escpos import printer 
+import utils
+import os
+# import time
+
+def get_existing_lp_files():
+    usb_dir = '/dev/usb'
+    lp_files = [os.path.join(usb_dir, file) for file in os.listdir(usb_dir) if file.startswith('lp')]
+    return lp_files
 
 def print_images(nb_models):
-    #Usb(0x416, 0x5011, in_ep=0x81, out_ep=0x03, profile="POS-5890")
-    printer = [
-        [0x416, 0x5011, 0x81, 0x03]
-    ]
-    nb_printer = 0
-    for i in range(nb_models):
-        f = f"print/{i}.png"
-        if printer[nb_printer] != None:
-            d = printer[nb_printer]
-            p = Usb(d[0], d[1], in_ep=d[2], out_ep=d[3], profile="POS-5890")
-            # Print the image
-            p.image(f)
+    try:
+        lp_files = get_existing_lp_files()
+
+        for i, path in enumerate(lp_files):
+            if i < nb_models:
+                try:
+                    p = printer.File(devfile=path)
+                    image_path = f"print/{i}.png"
+                    utils.rotate_image_180(image_path, "tmp.png")
+                    p.image('tmp.png')
+                except Exception as e:
+                    print(f"Error printing image {i}: {e}")
+                    continue
+        if os.path.exists("tmp.png"):
+            os.remove('tmp.png')
+            return
+
+    except Exception as e:
+                    print(f"Error printing: {e}")
+                    return
